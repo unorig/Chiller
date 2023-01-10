41451c17513ef4a8fe6b724d5cf17c8f3c36eba7
unorig <euangamble@gmail.com>
2023-01-10 19:51:58 +1100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-..\Assemblers\64tass\64tass.exe -a "chill.prg_64tass.S" -o "chill_compiled.prg"
+..\Assemblers\64tass\64tass.exe -a "chill.prg_64tass.asm" -o "chill_compiled.prg"
 ..\Tools\exomizer.exe sfx $0818 -x 1 "chill_compiled.prg" -o "chill_packed.prg" -p 1
-..\Emulators\C64Debugger\C64Debugger.exe -wait 2500 -autojmp -prg "chill_packed.prg"
+..\Emulators\RetroDebugger.exe -wait 2500 -autojmp -prg "chill_packed.prg"
